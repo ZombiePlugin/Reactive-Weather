3b48eacdff89c6f4f37184b51cfeea6d5b4dcefa
M <muzameelahmed1@gmail.com>
2023-11-15 13:05:32 +0000

    form submit

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import WeatherCard from "./components/WeatherCard";
 import Location from "./components/Location";
 import cities from "./data";
+import Form from "./components/Form";
 
 function App() {
   const [location, setLocation] = useState("Berlin");
@@ -12,6 +13,7 @@ function App() {
       <h1 className="title">REACTIVE WEATHER</h1>
       <h3 className="subtitle">Up to the minute weather news</h3>
       <div className="app">
+        <Form location={location} setLocation={setLocation} />
         {/* {cities.map((city) => (
           <WeatherCard city={city} />
         ))} */}
